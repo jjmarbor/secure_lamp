11a281b231ac24bf9269dfddf481dbc51ce3c2be
usuario <usuario@ubuntu-20.04-desktop-amd64.jesusamieiro.com>
2023-05-02 19:53:53 +0000

    Primer apartado ejercicio 1 SecurizeApache.

--- securizeApache.sh
+++ securizeApache.sh
@@ -7,21 +7,38 @@ echo "_\ \  __/ (__| |_| | |  | |/ /  __/ /  _  \/ ___/  _  \/ /___/ __  //__   
 echo "\__/\___|\___|\__,_|_|  |_/___\___| \_/ \_/\/   \_/ \_/\____/\/ /_/\__/   "
 echo ""
 
-# Usuarios y grupos de ejecución.
+#Variables
+ficheroVarEnt="/etc/apache2/envvars"
+rutaFicheroConfigApache="/etc/apache2/apache2.conf"
 
-echo "Vamos a comprobar si el usuario y el grupo del fichero de configuración de apache están igualados a 'www-data'."
-lineaUser=$(grep ^User.* /etc/apache2/apache2.conf)
+#1. Usuarios y grupos de ejecución.
+
+echo "Vamos a comprobar si el usuario y el grupo del fichero de configuración de apache están igualados a 'www-data': "
+lineaUser=$(grep ^User.* $rutaFicheroConfigApache)
 lineaUser=${lineaUser#*\{}
 lineaUser=${lineaUser%\}*}
 echo $lineaUser
 
-lineaGrupo=$(grep ^Group.* /etc/apache2/apache2.conf)
+lineaGrupo=$(grep ^Group.* $rutaFicheroConfigApache)
 lineaGrupo=${lineaGrupo#*\{}
 lineaGrupo=${lineaGrupo%\}*}
 echo $lineaGrupo
 
+if [[ $(grep ^$lineaUser=www-data $ficheroVarEnt) ]]
+then 
+	echo "El valor de la variable de entorno $lineaUser está establecida a www-data."
+else
+	sed -i 's/^User.*/User www-data/' $ficheroVarEnt
+	echo "El valor de la variable de entorno $lineaUser se ha establecido a www-data."
+fi
 
-
+if [[ $(grep ^$lineaGrupo=www-data $ficheroVarEnt) ]]
+then 
+	echo "El valor de la variable de entorno $lineaGrupo está establecida a www-data."
+else
+	sed -i 's/^Group.*/Group www-data/' $ficheroVarEnt
+	echo "El valor de la variable de entorno $lineaGrupo se ha establecido a www-data."
+fi
 
 
 
